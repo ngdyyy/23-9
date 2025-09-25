0ece6806f8d496a0df1e39cbba04ad8d683e32b2
ngdyyy <nqduy130206@gmail.com>
2025-09-25 07:54:40 +0700

    Add files via upload

---
+++ color.js
@@ -0,0 +1,11 @@
+const el = document.getElementById("name");
+el.style.color = "red";
+
+const al = document.getElementById("bod");
+al.style.color = "blue";
+
+const bl = document.getElementById("job");
+bl.style.color = "gray";
+
+const cl = document.getElementById("future");
+cl.style.color = "orange";
